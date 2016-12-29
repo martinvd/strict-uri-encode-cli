3bd218ed46d6d344112ce72baf0945791f167d43
Martin van Driel <martinvdpub@gmail.com>
2016-12-29 10:04:18 +0100

    Edit path

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ import test from 'ava';
 import execa from 'execa';
 
 test('main', async t => {
-  t.is(await execa.stdout('./cli.js', ['unicorn!foobar']), 'unicorn%21foobar');
-  t.is(await execa.stdout('./cli.js', ['unicorn\'foobar']), 'unicorn%27foobar');
-  t.is(await execa.stdout('./cli.js', ['unicorn*foobar']), 'unicorn%2Afoobar');
-  t.not(await execa.stdout('./cli.js', ['unicorn*foobar']), encodeURIComponent('unicorn*foobar'));
+  t.is(await execa.stdout('cli.js', ['unicorn!foobar']), 'unicorn%21foobar');
+  t.is(await execa.stdout('cli.js', ['unicorn\'foobar']), 'unicorn%27foobar');
+  t.is(await execa.stdout('cli.js', ['unicorn*foobar']), 'unicorn%2Afoobar');
+  t.not(await execa.stdout('cli.js', ['unicorn*foobar']), encodeURIComponent('unicorn*foobar'));
 });
 
 test('stdin', async t => {
-  t.is(await execa.stdout('./cli.js', {input: 'unicorn!foobar'}), 'unicorn%21foobar');
-  t.is(await execa.stdout('./cli.js', {input: 'unicorn\'foobar'}), 'unicorn%27foobar');
-  t.is(await execa.stdout('./cli.js', {input: 'unicorn*foobar'}), 'unicorn%2Afoobar');
-  t.not(await execa.stdout('./cli.js', {input: 'unicorn*foobar'}), encodeURIComponent('unicorn*foobar'));
+  t.is(await execa.stdout('cli.js', {input: 'unicorn!foobar'}), 'unicorn%21foobar');
+  t.is(await execa.stdout('cli.js', {input: 'unicorn\'foobar'}), 'unicorn%27foobar');
+  t.is(await execa.stdout('cli.js', {input: 'unicorn*foobar'}), 'unicorn%2Afoobar');
+  t.not(await execa.stdout('cli.js', {input: 'unicorn*foobar'}), encodeURIComponent('unicorn*foobar'));
 });
